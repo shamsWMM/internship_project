052c3132b47474e1a4cc6e2fdf7ce91a3fdad3b1
shamsWMM <101496933+shamsWMM@users.noreply.github.com>
2023-10-28 12:40:28 +1100

    Update server.sh

--- CloudLab/server.sh
+++ CloudLab/server.sh
@@ -1,3 +1,16 @@
 # Update package lists and install updates
 sudo apt update
 sudo apt -y full-upgrade
+
+# Get control and experiment interface names
+iface1=$(ifconfig | grep -B1 "inet 192.168.3.1" | head -n1 | cut -f1 -d:)
+iface2=$(ifconfig | grep -B1 "inet 192.168.4.1" | head -n1 | cut -f1 -d:)
+ifaceC=$(ifconfig | grep -B1 "inet " | head -n1 | cut -f1 -d:) 
+
+# Reset interfaces
+sudo ifconfig $iface1 down; sudo ifconfig $iface1 up 
+sudo ifconfig $iface2 down; sudo ifconfig $iface2 up
+
+# Add the new routes manually 
+sudo route add -net 192.168.10.0/24 gw 192.168.3.2 
+sudo route add -net 192.168.20.0/24 gw 192.168.4.2
