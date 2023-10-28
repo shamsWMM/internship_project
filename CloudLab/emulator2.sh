027f1f4239aabd69466fc4251c469d032d1727e6
shamsWMM <101496933+shamsWMM@users.noreply.github.com>
2023-10-28 12:04:22 +1100

    Update emulator2.sh

--- CloudLab/emulator2.sh
+++ CloudLab/emulator2.sh
@@ -6,5 +6,12 @@ sudo apt -y full-upgrade
 ifacetoC=$(ifconfig | grep -B1 "inet 192.168.20.1" | head -n1 | cut -f1 -d:)
 ifacetoS=$(ifconfig | grep -B1 "inet 192.168.2.1" | head -n1 | cut -f1 -d:)
 
+# Reset interfaces
+sudo ifconfig $ifacetoC down; sudo ifconfig $ifacetoC up 
+sudo ifconfig $ifacetoS down; sudo ifconfig $ifacetoS up
+
+# Add the new routes manually 
+sudo route add -net 192.168.4.0/24 gw 192.168.2.2
+
 # Introduce delay to the server link at the emulator 
 sudo tc qdisc replace dev $ifacetoS root netem delay 30ms limit 60000
