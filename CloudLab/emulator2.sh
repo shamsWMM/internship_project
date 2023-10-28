f894e6ae78056e9ad38ffdc9a4fb2a63e83f2c95
shamsWMM <101496933+shamsWMM@users.noreply.github.com>
2023-10-28 11:49:45 +1100

    Update emulator2.sh

--- CloudLab/emulator2.sh
+++ CloudLab/emulator2.sh
@@ -1,3 +1,10 @@
-#Update package lists and install updates
+# Update package lists and install updates
 sudo apt update
 sudo apt -y full-upgrade
+
+# get control and experiment interface names
+ifacetoC=$(ifconfig | grep -B1 "inet 192.168.20.1" | head -n1 | cut -f1 -d:)
+ifacetoS=$(ifconfig | grep -B1 "inet 192.168.2.1" | head -n1 | cut -f1 -d:)
+
+# Introduce delay to the server link at the emulator 
+sudo tc qdisc replace dev $ifacetoS root netem delay 30ms limit 60000
